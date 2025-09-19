6fd934fa1d381a6e86c35c66f083bdc8a8544fee
SergiooOliveira <sergioooliveira908@gmail.com>
2025-09-19 15:44:51 +0100

    Movement Speed

--- ProjetoAplicado/Assets/Assets/Scripts/Player/Player.cs
+++ ProjetoAplicado/Assets/Assets/Scripts/Player/Player.cs
@@ -5,13 +5,23 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     public static Player Instance;
-
+    public Rigidbody2D rb;
+    
+    
     private float movementX = 0f;
+    public float movementSpeed = 4f;
 
     public void Awake()
     {
         if (Instance != null) Destroy(gameObject);
         else Instance = this;
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+
     }
 
     public void OnMove(InputAction.CallbackContext callbackContext)
@@ -19,7 +29,7 @@ public class Player : MonoBehaviour
         if (callbackContext.performed)
         {
             //Debug.Log($"Carreguei no {callbackContext.ReadValue<Vector2>()}");
-            movementX = callbackContext.ReadValue<Vector2>().x;
+            movementX = callbackContext.ReadValue<Vector2>().x * movementSpeed;
         }
     }
 }
