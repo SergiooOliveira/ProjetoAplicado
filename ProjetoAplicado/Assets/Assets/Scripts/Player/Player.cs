652ce6fb009af1f9e7435249de312b2f75d93753
SergiooOliveira <sergioooliveira908@gmail.com>
2025-09-20 10:27:07 +0100

    It moves!!!!

--- ProjetoAplicado/Assets/Assets/Scripts/Player/Player.cs
+++ ProjetoAplicado/Assets/Assets/Scripts/Player/Player.cs
@@ -6,30 +6,63 @@ public class Player : MonoBehaviour
 {
     public static Player Instance;
     public Rigidbody2D rb;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
     
     
-    private float movementX = 0f;
-    public float movementSpeed = 4f;
+    private float horizontal;
+    private float movementSpeed = 4f;
+    public float jumpingPower = 16f;
+    private bool isFacingRight = true;
 
-    public void Awake()
+    #region Unity Methods
+    public void Awake ()
     {
         if (Instance != null) Destroy(gameObject);
         else Instance = this;
-
-        rb = GetComponent<Rigidbody2D>();
     }
 
-    private void FixedUpdate()
+    private void FixedUpdate ()
     {
+        rb.linearVelocity = new Vector2(horizontal * movementSpeed, rb.linearVelocity.y);
 
+        if (!isFacingRight && horizontal > 0f) Flip();
+        else if (isFacingRight && horizontal < 0f) Flip();
+    }
+    #endregion
+
+    #region Player Checks
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
 
-    public void OnMove(InputAction.CallbackContext callbackContext)
+    private void Flip()
     {
-        if (callbackContext.performed)
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
+    }
+    #endregion
+
+    #region Unity Events
+    public void OnMove (InputAction.CallbackContext callbackContext)
+    {
+        //Debug.Log($"Carreguei no {callbackContext.ReadValue<Vector2>()}");
+        horizontal = callbackContext.ReadValue<Vector2>().x;
+    }
+
+    public void OnJump (InputAction.CallbackContext callbackContext)
+    {
+        if (callbackContext.performed) Debug.Log($"Space Pressed");
+        if (IsGrounded()) Debug.Log($"Space Pressed");
+
+        if (callbackContext.performed && IsGrounded())
         {
-            //Debug.Log($"Carreguei no {callbackContext.ReadValue<Vector2>()}");
-            movementX = callbackContext.ReadValue<Vector2>().x * movementSpeed;
+            Debug.Log("Jumping");
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
         }
     }
+    #endregion
 }
