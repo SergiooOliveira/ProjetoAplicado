47561e1af2f643323aba8d8c1205e3d5c9f8e44d
SergiooOliveira <sergioooliveira908@gmail.com>
2025-10-04 12:24:12 +0100

    Enemy script, interface and scriptableObject

---
+++ Project/Assets/Assets/Scripts/Character/Enemy/EnemyData.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy / New Enemy")]
+public class EnemyData : ScriptableObject, IEnemy
+{
+    #region Serizalized Fields
+    [SerializeField] private string enemyName;
+    [SerializeField] private string enemyDescription;
+    [SerializeField] private EnemyType enemyType;
+    [SerializeField] private EnemySpawnLevel enemySpawnLevel;
+    [SerializeField] private Spell enemySpell;
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private Stat enemyHp = new Stat(100);
+    [SerializeField] private int enemyAttack;
+    [SerializeField] private int enemyDefense;
+    [SerializeField] private int enemyLevel;
+    [SerializeField] private List<Item> enemyDrops;
+    #endregion
+
+    #region Property implementation
+    public string EnemyName => enemyName;
+    public string EnemyDescription => enemyDescription;
+    public EnemyType EnemyType => enemyType;
+    public EnemySpawnLevel EnemySpawnLevel => enemySpawnLevel;
+    public Spell EnemySpell => enemySpell;
+    public GameObject EnemyPrefab => enemyPrefab;
+    public Stat EnemyHp => enemyHp;
+    public int EnemyAttack => enemyAttack;
+    public int EnemyDefense => enemyDefense;
+    public int EnemyLevel => enemyLevel;
+    public List<Item> EnemyDrops => enemyDrops;
+    #endregion
+
+    // TODO: Need to create an Element Class
+    public float GetResistance()
+    {
+        return 0;
+    }
+
+    public override string ToString()
+    {
+        string info = $"{EnemyName}: has {EnemyHp.Max} HP and {EnemyAttack} Damage";
+
+        return info;
+    }
+}
