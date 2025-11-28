e6b431e1f5dbd335432b9a3a5431985796e3af07
railks21 <josepedro.abreu21@gmail.com>
2025-11-28 22:13:53 +0000

    Lobby

--- Project/Assets/Assets/Scripts/Managers/MenuManager.cs
+++ Project/Assets/Assets/Scripts/Managers/MenuManager.cs
@@ -1,52 +1,131 @@
+//using FishNet;
+//using FishNet.Example;
+//using FishNet.Managing.Scened;
+//using System.Collections;
+//using System.Runtime.CompilerServices;
+//using UnityEngine;
+//using UnityEngine.InputSystem.HID;
+
+//public class MenuManager : MonoBehaviour
+//{
+//    #region Fields
+
+//    private bool isLoading = false;
+
+//    [Header("HUD")]
+//    public NetworkHudCanvases hud;
+
+//    #endregion
+
+//    #region Methods
+
+//    public void PlayButton()
+//    {
+//        if (isLoading)
+//            return;
+
+//        isLoading = true;
+
+//        //TSceneManager sm = GameObject.FindFirstObjectByType<TSceneManager>();
+
+//        //// First load the Loading screen
+//        //sm.LoadLoadingThenMap("Map1_Part1");
+
+//        BootstrapSceneManager sm = GameObject.FindFirstObjectByType<BootstrapSceneManager>();
+//        sm.UnloadScene("StartMenu");
+//        sm.LoadScene("Map1_Part1");
+//    }
+
+//    public void Multiplayer()
+//    {
+//        if (isLoading)
+//            return;
+
+//        isLoading = true;
+
+//        //TSceneManager sm = GameObject.FindFirstObjectByType<TSceneManager>();
+//        //sm.LoadMapByName("Lobby");
+//        BootstrapSceneManager sm = GameObject.FindFirstObjectByType<BootstrapSceneManager>();
+
+//        // First load the Loading screen
+//        sm.UnloadScene("StartMenu");
+//        sm.LoadScene("Lobby");
+//    }
+
+//    public void PlayMultiplayer()
+//    {
+//        if (isLoading) return;
+//        isLoading = true;
+
+//        if (!InstanceFinder.IsServerStarted) return;
+
+//        StartCoroutine(UnloadLobbyThenLoadMap());
+//    }
+
+//    private IEnumerator UnloadLobbyThenLoadMap()
+//    {
+//        // 1. Apenas o servidor inicia o unload
+//        SceneUnloadData sud = new SceneUnloadData("Lobby");
+//        InstanceFinder.SceneManager.UnloadGlobalScenes(sud);
+
+//        // 2. Espera 1 frame para garantir processamento do unload
+//        yield return null;
+
+//        // 3. Debug: verifique se o servidor já processou o unload
+//        Debug.Log("Lobby unload chamado pelo servidor");
+
+//        // 4. Espera 10 segundos antes de carregar o mapa
+//        yield return new WaitForSeconds(10f);
+
+//        // 5. Carrega mapa globalmente (clientes sincronizam automaticamente)
+//        SceneLoadData sld = new SceneLoadData("Map1_Part1");
+//        InstanceFinder.SceneManager.LoadGlobalScenes(sld);
+
+//        Debug.Log("Map1_Part1 carregada pelo servidor e clientes devem sincronizar");
+//    }
+
+//    public void QuitGame()
+//    {
+//        Application.Quit();
+//    }
+
+//    #endregion
+//}
+
 using FishNet;
 using FishNet.Example;
 using FishNet.Managing.Scened;
 using System.Collections;
-using System.Runtime.CompilerServices;
 using UnityEngine;
-using UnityEngine.InputSystem.HID;
 
 public class MenuManager : MonoBehaviour
 {
-    #region Fields
-
     private bool isLoading = false;
-
-    #endregion
-
-    #region Methods
+    [Header("HUD")]
+    public NetworkHudCanvases hud;
 
     public void PlayButton()
     {
-        if (isLoading)
-            return;
-
+        if (isLoading) return;
         isLoading = true;
 
-        //TSceneManager sm = GameObject.FindFirstObjectByType<TSceneManager>();
-
-        //// First load the Loading screen
-        //sm.LoadLoadingThenMap("Map1_Part1");
-
         BootstrapSceneManager sm = GameObject.FindFirstObjectByType<BootstrapSceneManager>();
-        sm.UnloadScene("StartMenu");
-        sm.LoadScene("Map1_Part1");
+        sm.UnloadScene("StartMenu", () =>
+        {
+            sm.LoadScene("Map1_Part1");
+        });
     }
 
     public void Multiplayer()
     {
-        if (isLoading)
-            return;
-
+        if (isLoading) return;
         isLoading = true;
 
-        //TSceneManager sm = GameObject.FindFirstObjectByType<TSceneManager>();
-        //sm.LoadMapByName("Lobby");
         BootstrapSceneManager sm = GameObject.FindFirstObjectByType<BootstrapSceneManager>();
-
-        // First load the Loading screen
-        sm.UnloadScene("StartMenu");
-        sm.LoadScene("Lobby");
+        sm.UnloadScene("StartMenu", () =>
+        {
+            sm.LoadScene("Lobby");
+        });
     }
 
     public void PlayMultiplayer()
@@ -54,35 +133,29 @@ public class MenuManager : MonoBehaviour
         if (isLoading) return;
         isLoading = true;
 
-        // Apenas servidor inicia o carregamento global
         if (!InstanceFinder.IsServerStarted) return;
 
-        // Unload Lobby
-        InstanceFinder.SceneManager.UnloadGlobalScenes(new SceneUnloadData("Lobby"));
+        BootstrapSceneManager sm = GameObject.FindFirstObjectByType<BootstrapSceneManager>();
 
-        // Load Map1_Part1
-        //SceneLoadData sld = new SceneLoadData("Map1_Part1");
-        //InstanceFinder.SceneManager.LoadGlobalScenes(sld);
-
-        StartCoroutine(LoadMapWithDelay(10f, "Map1_Part1"));
+        // Use o BootstrapSceneManager para executar a Coroutine segura
+        sm.UnloadScene("Lobby", () =>
+        {
+            sm.StartCoroutine(sm.LoadMapWithDelay(10f, "Map1_Part1"));
+        });
     }
 
-    private IEnumerator LoadMapWithDelay(float delay, string mapName)
+    public IEnumerator LoadMapWithDelay(float delay, string mapName)
     {
-        // Aguarda o tempo especificado
         yield return new WaitForSeconds(delay);
 
-        // Carrega a nova cena
         SceneLoadData sld = new SceneLoadData(mapName);
         InstanceFinder.SceneManager.LoadGlobalScenes(sld);
 
-        Debug.Log($"{mapName} foi carregada após {delay} segundos.");
+        Debug.Log($"[{nameof(BootstrapSceneManager)}] {mapName} carregada após {delay} segundos.");
     }
 
     public void QuitGame()
     {
         Application.Quit();
     }
-
-    #endregion
 }
