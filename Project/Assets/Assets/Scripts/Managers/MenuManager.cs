13de5d2d2b5d0096f49cdc77411c98da5c35ed3e
railks21 <josepedro.abreu21@gmail.com>
2025-12-04 15:39:30 +0000

    Lobby

--- Project/Assets/Assets/Scripts/Managers/MenuManager.cs
+++ Project/Assets/Assets/Scripts/Managers/MenuManager.cs
@@ -67,7 +67,7 @@ public class MenuManager : MonoBehaviour
         var sm = GameObject.FindFirstObjectByType<BootstrapSceneManager>();
 
         sm.UnloadScene("Lobby");
-        sm.LoadLoadingThenMap("Map1_Part1");
+        sm.LoadScene("Map1_Part1");
     }
 
     #endregion
